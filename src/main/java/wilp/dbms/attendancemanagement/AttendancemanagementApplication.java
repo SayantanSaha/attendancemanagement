c720538ccdbe36ab440597f9c48c104282222f55
snehasil93 <snehasil2009@gmail.com>
2022-09-20 14:14:08 +0530

    first commit

---
+++ src/main/java/wilp/dbms/attendancemanagement/AttendancemanagementApplication.java
@@ -0,0 +1,13 @@
+package wilp.dbms.attendancemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendancemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendancemanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wilp/dbms/attendancemanagement/AttendancemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package wilp.dbms.attendancemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendancemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
