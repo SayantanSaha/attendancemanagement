456dc8a0f4f6e10b2c2e8c6ac2a0f1cc89d386a8
NIKILESH SAI <nikileshsai.rokkam@gmail.com>
2022-10-01 23:35:59 +0530

    Angular frontend implementation

---
+++ AngularApp/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent {
+  title = 'Attendance app';
+  loggedIn: boolean = false;
+  mobileQuery: MediaQueryList;
+  isShowing: boolean = true;
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+}
