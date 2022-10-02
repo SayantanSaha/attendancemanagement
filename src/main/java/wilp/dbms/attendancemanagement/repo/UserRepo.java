eba625f6daa6a517a997ac4f3fc9662bf14612b5
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2022-10-02 22:08:55 +0530

    CORS Allow All

--- src/main/java/wilp/dbms/attendancemanagement/repo/UserRepo.java
+++ src/main/java/wilp/dbms/attendancemanagement/repo/UserRepo.java
@@ -2,9 +2,11 @@ package wilp.dbms.attendancemanagement.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import wilp.dbms.attendancemanagement.model.User;
 
+@CrossOrigin("*")
 @RepositoryRestResource(collectionResourceRel = "user", path = "user")
 public interface UserRepo extends JpaRepository<User, Integer> {
 
